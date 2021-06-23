e1206eda984d02351e4a922800d7f19db647069d
ReAlpha39 <yunusw718@gmail.com>
2021-06-23 11:46:01 +0800

    add view for barang table

--- app/Http/Livewire/CreateBarang.php
+++ app/Http/Livewire/CreateBarang.php
@@ -1,28 +1,56 @@
 <?php
 
 namespace App\Http\Livewire;
+
 use App\Models\Barang;
 
 use Livewire\Component;
 
 class CreateBarang extends Component
 {
+    public $selected_id;
+    public $barang;
     public $nama_barang;
     public $jumlah_barang;
     public $tgl_masuk;
     public $tgl_kadaluarsa;
     public $harga;
     public $kategori;
+    public $updateMode = false;
+    public $isOpen = false;
 
     public function render()
     {
+        $this->barang = Barang::all();
         return view('livewire.create-barang');
+    }
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->resetInput();
+        $this->updateMode = false;
+        $this->isOpen = false;
+    }
+
+    private function resetInput()
+    {
+        $this->nama_barang = "";
+        $this->jumlah_barang = "";
+        $this->tgl_masuk = "";
+        $this->tgl_kadaluarsa = "";
+        $this->harga = "";
+        $this->kategori = "";
     }
 
     public function createBarang()
     {
         Barang::create([
-            'nama_barang' => $this -> nama_barang,
+            'nama_barang' => $this->nama_barang,
             'jumlah_barang' => $this->jumlah_barang,
             'tgl_masuk' => $this->tgl_masuk,
             'tgl_kadaluarsa' => $this->tgl_kadaluarsa,
@@ -30,12 +58,66 @@ class CreateBarang extends Component
             'kategori' => $this->kategori
         ]);
 
-        $this -> nama_barang = "";
-        $this->jumlah_barang = "";
-        $this->tgl_masuk = "";
-        $this->tgl_kadaluarsa = "";
-        $this->harga = "";
-        $this->kategori = "";
+        $this->closeModal();
+        session()->flash('message', 'Data berhasil ditambah');
+    }
 
+    public function edit($id)
+    {
+        $record = Barang::findOrFail($id);
+        $this->selected_id = $id;
+        $this->nama_barang = $record->nim;
+        $this->jumlah_barang = $record->nama;
+        $this->alamat = $record->alamat;
+
+        $this->nama_barang = $record->nama_barang;
+        $this->jumlah_barang = $record->jumlah_barang;
+        $this->tgl_masuk = $record->tgl_masuk;
+        $this->tgl_kadaluarsa = $record->tgl_kadaluarsa;
+        $this->harga = $record->harga;
+        $this->kategor = $record->kategor;
+
+        $this->updateMode = true;
+        $this->openModal();
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'jumlah_barang' => 'required',
+            'jumlah_barang' => 'required',
+            'tgl_masuk' => 'required',
+            'tgl_kadaluarsa' => 'required',
+            'harga' => 'required',
+            'kategori' => 'required',
+        ]);
+
+        if ($this->selected_id) {
+            $record = Barang::find($this->selected_id);
+            $record->update([
+                'jumlah_barang' => $this->jumlah_barang,
+                'jumlah_barang' => $this->jumlah_barang,
+                'tgl_masuk' => $this->tgl_masuk,
+                'tgl_kadaluarsa' => $this->tgl_kadaluarsa,
+                'harga' => $this->harga,
+                'kategori' => $this->kategori,
+            ]);
+
+            $this->closeModal();
+            session()->flash('message', 'Data berhasil di update');
+        }
+    }
+
+    public function delete($id)
+    {
+        if ($id) {
+            Barang::find($id)->delete();
+            session()->flash('message', 'Data berhasil dihapus');
+        }
+
+        if ($this->updateMode) {
+            $this->resetInput();
+            $this->updateMode = false;
+        }
     }
 }
---
+++ resources/views/livewire/input-barang.blade.php
@@ -0,0 +1,57 @@
+<div>
+    <div class="mt-10 sm:mt-0">
+        <div class="md:grid md:gap-6">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form>
+                    <div class="sm:rounded-md">
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <div class="grid grid-cols-1 gap-6">
+                                @if ($updateMode)
+                                    <input type="hidden" wire:model="selected_id">
+                                @endif
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="nama_barang" class="block text-sm font-medium text-gray-700">
+                                        Nama Barang
+                                    </label>
+                                    <input wire:model="nama_barang" type="text" name="nama_barang" id="nama_barang" autocomplete="nama_barang"
+                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="jumlah_barang" class="block text-sm font-medium text-gray-700">
+                                        Jumlah Barang
+                                    </label>
+                                    <input wire:model="jumlah_barang" type="text" name="jumlah_barang" id="jumlah_barang" autocomplete="jumlah_barang"
+                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="tgl_masuk" class="block text-sm font-medium text-gray-700">
+                                        Tanggal Masuk
+                                    </label>
+                                    <input wire:model="tgl_masuk" type="text" name="tgl_masuk" id="tgl_masuk" autocomplete="tgl_masuk"
+                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button wire:click="closeModal()" type="button"
+                                class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                Cancel
+                            </button>
+                            @if ($updateMode)
+                                <button wire:click="update" type="button"
+                                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                    Update
+                                </button>
+                            @else
+                                <button wire:click.prevent="createBarang" type="button"
+                                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                    Save
+                                </button>
+                            @endif
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
