9a1e14e29cb6898b176c0e3074ba106cd866ddfd
ReAlpha39 <yunusw718@gmail.com>
2021-06-23 14:55:44 +0800

    rename dashboard to Barang

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/barang', function () {
+    return view('barang');
+})->name('barang');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/pegawai', function () {
     return view('pegawai');
