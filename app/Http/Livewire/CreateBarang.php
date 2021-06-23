9aad32425e864925ae23f33c82dd34a2cd5c7dba
ReAlpha39 <yunusw718@gmail.com>
2021-06-23 10:47:09 +0800

    add function to create barang

--- app/Http/Livewire/CreateBarang.php
+++ app/Http/Livewire/CreateBarang.php
@@ -1,13 +1,41 @@
 <?php
 
 namespace App\Http\Livewire;
+use App\Models\Barang;
 
 use Livewire\Component;
 
 class CreateBarang extends Component
 {
+    public $nama_barang;
+    public $jumlah_barang;
+    public $tgl_masuk;
+    public $tgl_kadaluarsa;
+    public $harga;
+    public $kategori;
+
     public function render()
     {
         return view('livewire.create-barang');
+    }
+
+    public function createBarang()
+    {
+        Barang::create([
+            'nama_barang' => $this -> nama_barang,
+            'jumlah_barang' => $this->jumlah_barang,
+            'tgl_masuk' => $this->tgl_masuk,
+            'tgl_kadaluarsa' => $this->tgl_kadaluarsa,
+            'harga' => $this->harga,
+            'kategori' => $this->kategori
+        ]);
+
+        $this -> nama_barang = "";
+        $this->jumlah_barang = "";
+        $this->tgl_masuk = "";
+        $this->tgl_kadaluarsa = "";
+        $this->harga = "";
+        $this->kategori = "";
+
     }
 }
--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+
+    protected $fillable =
+    [
+        'nama_barang',
+        'jumlah_barang',
+        'tgl_masuk',
+        'tgl_kadaluarsa',
+        'harga',
+        'kategori'
+    ];
+
 }
